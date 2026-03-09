0114dbd621f1575a19d09bf85f4664c57edfaf3e
Krishti Das <143172045+KrishtiDas@users.noreply.github.com>
2026-03-09 20:29:52 +0530

    Update sketch.py (the code)

--- code/sketch.py
+++ code/sketch.py
@@ -1,1 +1,14 @@
+import cv2
 
+image = cv2.imread("../sample_images/input.jpg")
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+inverted = 255 - gray_image
+blur = cv2.GaussianBlur(inverted, (21,21),0)
+
+invertedblur = 255 - blur
+
+sketch = cv2.divide(gray_image, invertedblur, scale=256.0)
+
+cv2.imwrite("../sample_images/output.png", sketch)
